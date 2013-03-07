35dcef8e782191251317217d3a304c41276a645c
Sophia Nazarova <sophia.nazarova@gmail.com>
2013-03-07 22:09:23 +0400

    size.str estuary script

--- White_Sea/Estuatiy_Luvenga/size_str.r
+++ White_Sea/Estuatiy_Luvenga/size_str.r
@@ -1,9 +1,11 @@
+#setwd("~/Dropbox/PhD_thesis/White_Sea/Estuatiy_Luvenga")
+
 # размерная структура суммарно по годам по горизонтам
 ishodnik<-read.table(file="length.csv", sep=";", dec=",", head=T)
 samples.squares<-read.table(file="squares.csv", sep=";", dec=",", head=T)
 samples.names<-read.table(file="sample.csv", sep=";", dec=",", head=T)
 attach(ishodnik)
-year<-factor(year)
+#year<-factor(year)
 
 Length.int<-cut(Length.mm, breaks=seq(0,20,1))
 
@@ -11,16 +13,17 @@ Length.int<-cut(Length.mm, breaks=seq(0,20,1))
 
 size.str.df<-as.data.frame(size.str.table) # как таблица данных
 
+#убираем те пробы которых на самом деле нету
+for (i in 1:length(levels(size.str.df$year)))
+{ (xxx<-size.str.df$sample[size.str.df$year==levels(size.str.df$year)[i] ]%in%
+     samples.names$sample[samples.names$year==levels(size.str.df$year)[i]])
+  antixxx<-as.logical(1-xxx)
+  size.str.df$Freq[size.str.df$year==levels(size.str.df$year)[i]][antixxx]<-NA
+}
+
+
 # SUBSET - для фильтрации таблицы данных
 # APPLY - кто-то из них для средней и СД по фрейму
-# все что не сушествует надо сделать NA
-# ЭТО НЕ РАБОТАЕТ??
-#for (i in 1:length(levels(size.str.df$year)))
-#{
-#  size.str.df$Freq[size.str.df$year==levels(size.str.df$year)[i] & size.str.df$sample!=samples.names$sample[samples.names$year==levels(size.str.df$year)[i]]]<-NA
-#}
-
-size.str.df<-subset(size.str.df, size.str.df$Freq!="NA")
 
 #теперь на квадратный метр
 size.str.sqmeter<-size.str.df
@@ -29,24 +32,38 @@ for (i in 1:length(levels(size.str.sqmeter$year)))
   size.str.sqmeter$Freq[size.str.sqmeter$year==levels(size.str.sqmeter$year)[i]]<-size.str.sqmeter$Freq[size.str.sqmeter$year==levels(size.str.sqmeter$year)[i]] * samples.squares$square[samples.squares$year==levels(size.str.sqmeter$year)[i]]
 }
 
-subset(size.str.sqmeter,subset=size.str.sqmeter$year=="1992")
+#subset(size.str.sqmeter,subset=size.str.sqmeter$year=="1992")
+
 #и среднее??
 # tapply выдает как резудьтат матрицу
-(mean.sizestr.sqmeter<-tapply(size.str.sqmeter$Freq,INDEX=list(size.str.sqmeter$year, size.str.sqmeter$Length.int),FUN=mean))
+(mean.sizestr.sqmeter<-t(tapply(size.str.sqmeter$Freq,INDEX=list(size.str.sqmeter$year, size.str.sqmeter$Length.int),FUN=mean, na.rm=T)))
+mean.sqmeter.df<-as.data.frame(mean.sizestr.sqmeter)
+(sd.sizestr.sqmeter<-tapply(size.str.sqmeter$Freq,INDEX=list(size.str.sqmeter$year, size.str.sqmeter$Length.int),FUN=sd, na.rm=T))
 
-  (sd.sizestr.sqmeter<-tapply(size.str.sqmeter$Freq,INDEX=list(size.str.sqmeter$year, size.str.sqmeter$Length.int),FUN=))
+n.samples<-tapply(samples.names$sample,samples.names$year, length )
 
-sem.sizestr.sqmeter <-sd.sizestr.sqmeter
+(sem.sizestr.sqmeter <-t(sd.sizestr.sqmeter/sqrt(as.vector(n.samples))))
+sem.sqmeter.df<-as.data.frame(sem.sizestr.sqmeter)
 
-
-(mean.size.str.df<-as.data.frame(size.str.table))
 length.class<-seq(1,20,1)
 
-for (i in 1:length(levels(size.str.df$tidal_zone)))
-{
-  for (j in 1:length(levels(size.str.df$year)))
+#from R-book 
+error.bars<-function(yv,z,nn){
+  xv<-
+    barplot(yv,ylim=c(0,(max(yv)+max(z))),names=nn)#,ylab=deparse(substitute(yv)))
+  g=(max(xv)-min(xv))/50
+  for (i in 1:length(xv)) {
+    lines(c(xv[i],xv[i]),c(yv[i]+z[i],yv[i]-z[i]))
+    lines(c(xv[i]-g,xv[i]+g),c(yv[i]+z[i], yv[i]+z[i]))
+    lines(c(xv[i]-g,xv[i]+g),c(yv[i]-z[i], yv[i]-z[i]))
+  }}
+
+
+  for (j in 1:length(colnames(mean.sizestr.sqmeter)))
   {
-    pdf(file=paste("sizestr", size.str.df$tidal_zone[i], size.str.df$year[j], ".pdf",sep="_"),width=1000, height=790)
-    barplot(size.str.df$Freq[size.str.df$tidal_zone==paste(size.str.df$tidal_zone[i]) & size.str.df$year==paste(size.str.df$year[j])], names.arg=length.class)
+    pdf(file=paste("sizestr", colnames(mean.sizestr.sqmeter)[j], ".pdf",sep="_"), paper="a4")
+    error.bars(yv=mean.sizestr.sqmeter[,j], nn=length.class, z=sem.sizestr.sqmeter[,j])
+    title(main=colnames(mean.sizestr.sqmeter)[j], xlab="", ylab="")
     dev.off()
-}}
+  }
+
