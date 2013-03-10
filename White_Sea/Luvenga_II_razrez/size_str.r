51f4cca81c7ac232290d4e0b295d08c8069eabe0
Sophia Nazarova <sophia.nazarova@gmail.com>
2013-03-10 09:20:10 +0400

    estuary pictures, II razrez scriot start

---
+++ White_Sea/Luvenga_II_razrez/size_str.r
@@ -0,0 +1,72 @@
+setwd("~/Dropbox/PhD_thesis/White_Sea/Luvenga_II_razrez/")
+
+# размерная структура суммарно по годам по горизонтам
+ishodnik<-read.table(file="length.csv", sep=";", dec=",", head=T)
+samples.squares<-read.table(file="squares.csv", sep=";", dec=",", head=T)
+samples.names<-read.table(file="sample.csv", sep=";", dec=",", head=T)
+attach(ishodnik)
+#year<-factor(year)
+
+Length.int<-cut(Length.mm, breaks=seq(0,20,1))
+
+(size.str.table<-table(Length.int,year, tidal_level, sample))
+
+size.str.df<-as.data.frame(size.str.table) # как таблица данных
+
+#убираем те пробы которых на самом деле нету
+for (i in 1:length(levels(size.str.df$year)))
+{ (xxx<-size.str.df$sample[size.str.df$year==levels(size.str.df$year)[i] ]%in%
+     samples.names$sample[samples.names$year==levels(size.str.df$year)[i]])
+  antixxx<-as.logical(1-xxx)
+  size.str.df$Freq[size.str.df$year==levels(size.str.df$year)[i]][antixxx]<-NA
+}
+
+
+# SUBSET - для фильтрации таблицы данных
+# APPLY - кто-то из них для средней и СД по фрейму
+
+#теперь на квадратный метр
+size.str.sqmeter<-size.str.df
+for (i in 1:length(levels(size.str.sqmeter$year)))
+{
+  size.str.sqmeter$Freq[size.str.sqmeter$year==levels(size.str.sqmeter$year)[i]]<-
+    size.str.sqmeter$Freq[size.str.sqmeter$year==levels(size.str.sqmeter$year)[i]] * 
+    samples.squares$square[samples.squares$year==levels(size.str.sqmeter$year)[i]]
+}
+
+#subset(size.str.sqmeter,subset=size.str.sqmeter$year=="1992")
+
+#и среднее??
+# tapply выдает как резудьтат матрицу
+(mean.sizestr.sqmeter<-tapply(size.str.sqmeter$Freq,INDEX=list(size.str.sqmeter$year, size.str.sqmeter$tidal_level, size.str.sqmeter$Length.int),FUN=mean, na.rm=T))
+mean.sqmeter.df<-as.data.frame(mean.sizestr.sqmeter)
+(sd.sizestr.sqmeter<-tapply(size.str.sqmeter$Freq,INDEX=list(size.str.sqmeter$year, size.str.sqmeter$tidal_level,  size.str.sqmeter$Length.int),FUN=sd, na.rm=T))
+
+(n.samples<-tapply(samples.names$sample,list(samples.names$year,samples.names$tidal.level), length ))
+n.samples.df<-as.data.frame(n.samples)
+
+(sem.sizestr.sqmeter <-t(sd.sizestr.sqmeter/sqrt(n.samples)))
+sem.sqmeter.df<-as.data.frame(sem.sizestr.sqmeter)
+
+length.class<-seq(1,20,1)
+
+#from R-book 
+error.bars<-function(yv,z,nn){
+  xv<-
+    barplot(yv,ylim=c(0,(max(yv)+max(z))),names=nn)#,ylab=deparse(substitute(yv)))
+  g=(max(xv)-min(xv))/50
+  for (i in 1:length(xv)) {
+    lines(c(xv[i],xv[i]),c(yv[i]+z[i],yv[i]-z[i]))
+    lines(c(xv[i]-g,xv[i]+g),c(yv[i]+z[i], yv[i]+z[i]))
+    lines(c(xv[i]-g,xv[i]+g),c(yv[i]-z[i], yv[i]-z[i]))
+  }}
+
+
+  for (j in 1:length(colnames(mean.sizestr.sqmeter)))
+  {
+    pdf(file=paste("sizestr", colnames(mean.sizestr.sqmeter)[j], ".pdf",sep="_"), paper="a4")
+    error.bars(yv=mean.sizestr.sqmeter[,j], nn=length.class, z=sem.sizestr.sqmeter[,j])
+    title(main=colnames(mean.sizestr.sqmeter)[j], xlab="", ylab="")
+    dev.off()
+  }
+
